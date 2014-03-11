b8ad0d9c8629bcb163532a57a0dd8ec8342b8236
fujidig <fuji.rosen@gmail.com>
2014-03-11 16:24:47 +0900

    node.d.tsを使うのをやめる

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-﻿///<reference path="node.d.ts"/>
-///<reference path="axdata.ts"/>
+﻿///<reference path="axdata.ts"/>
 ///<reference path="compiler.ts"/>
 
 // how to run:  tsc app.ts --out app.js && node app.js
 
+declare var require, process;
 var fs = require("fs");
 var binary = fs.readFileSync(process.argv[2] || "hsp-programs/hello/hello.ax").toString("binary");
 var axdata = new BitterHSP.AXData(binary);
